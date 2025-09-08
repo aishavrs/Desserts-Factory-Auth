fc7eead0a561b5316e07b88a84baafcbd945f226
aishavrs <aishaashiruu@gmail.com>
2025-09-08 12:28:05 +0100

    mongo uri fix

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
+
 const dotenv = require("dotenv")
+dotenv.config()
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 // const port = 5000;
 const cors = require("cors");
 
-
-dotenv.config()
 const userRoutes = require("./routes/userRouter")
 //middlewares
 app.use(express.json())
@@ -20,7 +20,7 @@ app.use("/api/user", userRoutes)
 
 const startServer = async ()=>{
     try {
-        await mongoose.connect(process.env.mongo_url)
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("MongoDB connected")
 
         const port = process.env.PORT;
